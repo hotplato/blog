a4119dd125456ae2d0ac3408aff3720985e5ecf5
hotplato <734824592@qq.com>
2019-01-08 18:50:46 +0800

    建立基本结构

---
+++ docs/.vuepress/components/tagList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <ul v-for="post of tags" :key="post.key">
+        <li>
+            <router-link :to="{path:post.path}">{{post.title}}</router-link>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    tags() {
+      let tags = [];
+      for (const page of this.$site.pages) {
+        if (page.path.indexOf('_post') > -1) {
+          tags.push(page);
+        }
+      }
+      return tags;
+    }
+  }
+};
+</script>
+
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,5 @@
 module.exports = {
-    title: '柏拉图的饭堂',
-    description: '这里是柏拉图的饭堂',
-    themeConfig: {
-        lastUpdated: '最后编辑时间', // string | boolean
-    },
-    plugins: ['@vuepress/blog', '@vuepress/last-updated', {
-        transformer: (timestamp, lang) => {
-            console.log('测试:', timestamp)
-            return timestamp
-        }
-    }]
+    title: '柏拉图笔记',
+    description: '这里是柏拉图的笔记',
+    plugins: ['@vuepress/blog']
 }
