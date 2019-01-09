06cfa08b4408752d1efb42f0e4febacfb712f438
hotplato <734824592@qq.com>
2019-01-09 11:51:02 +0800

    修改脚本

--- script/build.sh
+++ script/build.sh
@@ -7,7 +7,8 @@ npm run build
 cd ./docs/.vuepress/dist
 
 git init
+git config user.name "${username}"
+git config user.email "${useremail}"
 git add -A
 git commit -m 'deploy'
-
-git push -f git@github.com:ning734824592/blog.git master:gh-pages
+git push -f https://${token}@github.com/ning734824592/blog.git master:gh-pages
