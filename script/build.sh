5d6b51ff34e843492cfebee67b0b19c5ee8c73c7
yanfei <feifeiwoye@outlook.com>
2019-05-08 15:55:28 +0800

    更改

--- script/build.sh
+++ script/build.sh
@@ -11,6 +11,6 @@ git config user.name "${username}"
 git config user.email "${useremail}"
 git add -A
 git commit -m 'deploy'
-# git push -f https://${token}@github.com/hotplato/blog.git master:gh-pages
+git push -f https://${token}@github.com/hotplato/hotplato.github.io.git master
 # git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-git push -f git@github.com:hotplato/hotplato.github.io.git master
+# git push -f git@github.com:hotplato/hotplato.github.io.git master
